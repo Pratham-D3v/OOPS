3ed56d15c9905fd0b26a287fa17dadc13bf782c1
Pratham-D3v <prathamdev2006@gmail.com>
2026-02-23 09:18:26 +0530

    Print OOPS using Static Function

---
+++ UC6.java
@@ -0,0 +1,52 @@
+public class UC6 {
+
+    // Pattern for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    // Pattern for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Pattern for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+            "*****",
+            "*    ",
+            "*****",
+            "    *",
+            "*****"
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Assemble banner row by row
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+            );
+        }
+    }
+}
